21606c276fc7678831a24344182b600f40d3f8ae
Mike Bova <mbova407@yahoo.com>
2023-12-04 09:05:14 -0500

    End of Day 3

---
+++ JavaScript/Day3/day3-part1.js
@@ -0,0 +1,83 @@
+import TextInputReader from '../Helpers/TextInputReader.js';
+import path from 'path';
+import { start } from 'repl';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileName = path.join(__dirname, 'data.data');
+
+const input = TextInputReader(fileName);
+
+const partNumber = [];
+const symbols = [];
+
+function sumHits(x, y, partNumber) {
+    var results = 0;
+    partNumber.forEach(part => {
+        if (x >= part.x && x <= part.maxX && y === part.y && !part.isCounted) {
+            part.isCounted = true;
+            results += part.number;
+        }
+    });
+    return results;
+}
+input.forEach((line, l) => {
+    let number = '';
+    let startNumber = -1;
+    for (var i = 0; i < line.length; i++) {
+        if (line[i].match(/\d/)) {
+            number += line[i];
+            if (startNumber === -1) startNumber = i;
+            continue;
+        }
+
+        // if it was not a number then save off the number if it exists 
+        if (number !== '') {
+            partNumber.push({ "x": startNumber, "maxX": startNumber + number.length - 1, "y": l, "number": +number, "isCounted": false });
+            number = '';
+            startNumber = -1;
+        }
+        if (line[i] !== '.') {
+            symbols.push({ "x": i, "y": l });
+        }
+    }
+
+    //end of line check
+    if (number !== '') {
+        partNumber.push({ "x": startNumber, "maxX": startNumber + number.length - 1, "y": l, "number": +number, "isCounted": false });
+        number = '';
+        startNumber = -1;
+    }
+});
+
+
+
+
+
+var answer = 0;
+symbols.forEach(coordinates => {
+    // top-left
+    answer += sumHits(coordinates.x - 1, coordinates.y - 1, partNumber);
+    // top-middle
+    answer += sumHits(coordinates.x, coordinates.y - 1, partNumber);
+    // top-right
+    answer += sumHits(coordinates.x + 1, coordinates.y - 1, partNumber);
+    // middle-left
+    answer += sumHits(coordinates.x - 1, coordinates.y, partNumber);
+    // middle-right
+    answer += sumHits(coordinates.x + 1, coordinates.y, partNumber);
+    // bottom-left
+    answer += sumHits(coordinates.x - 1, coordinates.y + 1, partNumber);
+    // bottom-left
+    answer += sumHits(coordinates.x, coordinates.y + 1, partNumber);
+    // bottom-left
+    answer += sumHits(coordinates.x + 1, coordinates.y + 1, partNumber);
+
+});
+
+console.log(partNumber);
+console.log(symbols);
+
+console.log(answer);
+
---
+++ JavaScript/Day3/day3-part2.js
@@ -0,0 +1,88 @@
+import TextInputReader from '../Helpers/TextInputReader.js';
+import path from 'path';
+import { start } from 'repl';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileName = path.join(__dirname, 'data.data');
+
+const input = TextInputReader(fileName);
+
+const partNumber = [];
+const symbols = [];
+
+function findParts(x, y, partNumber) {
+
+    return partNumber.filter(part => {
+        if (x >= part.x && x <= part.maxX && y === part.y && !part.isCounted) {
+            part.isCounted = true;
+            return part;
+        }
+    });
+
+}
+input.forEach((line, l) => {
+    let number = '';
+    let startNumber = -1;
+    for (var i = 0; i < line.length; i++) {
+        if (line[i].match(/\d/)) {
+            number += line[i];
+            if (startNumber === -1) startNumber = i;
+            continue;
+        }
+
+        // if it was not a number then save off the number if it exists 
+        if (number !== '') {
+            partNumber.push({ "x": startNumber, "maxX": startNumber + number.length - 1, "y": l, "number": +number, "isCounted": false });
+            number = '';
+            startNumber = -1;
+        }
+        if (line[i] === '*') {
+            symbols.push({ "x": i, "y": l });
+        }
+    }
+
+    //end of line check
+    if (number !== '') {
+        partNumber.push({ "x": startNumber, "maxX": startNumber + number.length - 1, "y": l, "number": +number, "isCounted": false });
+        number = '';
+        startNumber = -1;
+    }
+});
+
+
+
+
+
+var answer = 0;
+symbols.forEach(coordinates => {
+    let results = [];
+    // top-left
+    results.push(...findParts(coordinates.x - 1, coordinates.y - 1, partNumber));
+    // top-middle
+    results.push(...findParts(coordinates.x, coordinates.y - 1, partNumber));
+    // top-right
+    results.push(...findParts(coordinates.x + 1, coordinates.y - 1, partNumber));
+    // middle-left
+    results.push(...findParts(coordinates.x - 1, coordinates.y, partNumber));
+    // middle-right
+    results.push(...findParts(coordinates.x + 1, coordinates.y, partNumber));
+    // bottom-left
+    results.push(...findParts(coordinates.x - 1, coordinates.y + 1, partNumber));
+    // bottom-left
+    results.push(...findParts(coordinates.x, coordinates.y + 1, partNumber));
+    // bottom-left
+    results.push(...findParts(coordinates.x + 1, coordinates.y + 1, partNumber));
+
+    if (results.length == 2) {
+        answer += results[0].number * results[1].number;
+    }
+
+    console.log(results, answer);
+});
+
+
+
+console.log(answer);
+
